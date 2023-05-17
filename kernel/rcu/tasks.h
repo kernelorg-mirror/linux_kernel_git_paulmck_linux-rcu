812195e4f70ef0e9fc68127355c553c537749546
Paul E. McKenney <paulmck@kernel.org>
2023-05-17 05:01:28 -0700

    rcuscale: Measure grace-period kthread CPU time
    This commit adds the ability to output the CPU time consumed by the
    grace-period kthread for the RCU variant under test.  The CPU time is
    whatever is in the designated task's current->stime field, and thus is
    controlled by whatever CPU-time accounting scheme is in effect.
    
    This output appears in microseconds as follows on the console:
    
    rcu_scale: Grace-period kthread CPU time: 42367.037
    
    [ paulmck: Apply feedback from Stephen Rothwell and kernel test robot. ]
    
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- kernel/rcu/tasks.h
+++ kernel/rcu/tasks.h
@@ -1830,6 +1830,14 @@ void show_rcu_tasks_trace_gp_kthread(void)
 EXPORT_SYMBOL_GPL(show_rcu_tasks_trace_gp_kthread);
 #endif // !defined(CONFIG_TINY_RCU)
 
+#ifdef CONFIG_RCU_SCALE_TEST
+struct task_struct *get_rcu_tasks_trace_gp_kthread(void)
+{
+	return rcu_tasks_trace.kthread_ptr;
+}
+EXPORT_SYMBOL_GPL(get_rcu_tasks_trace_gp_kthread);
+#endif // #ifdef CONFIG_RCU_SCALE_TEST
+
 #else /* #ifdef CONFIG_TASKS_TRACE_RCU */
 static void exit_tasks_rcu_finish_trace(struct task_struct *t) { }
 #endif /* #else #ifdef CONFIG_TASKS_TRACE_RCU */
