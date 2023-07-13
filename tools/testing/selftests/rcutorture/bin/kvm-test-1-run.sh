08365f14a8674de721e0a3bdd6953d3be88b48b9
Paul E. McKenney <paulmck@kernel.org>
2023-07-13 16:50:03 -0700

    fixup! torture: Avoid torture-test reboot loops

--- tools/testing/selftests/rcutorture/bin/kvm-test-1-run.sh
+++ tools/testing/selftests/rcutorture/bin/kvm-test-1-run.sh
@@ -9,9 +9,10 @@
 #
 # Usage: kvm-test-1-run.sh config resdir seconds qemu-args boot_args_in
 #
-# qemu-args defaults to "-enable-kvm -nographic", along with arguments
-#			specifying the number of CPUs and other options
-#			generated from the underlying CPU architecture.
+# qemu-args defaults to "-enable-kvm -nographic -no-reboot", along with
+#			arguments specifying the number of CPUs and
+#			other options generated from the underlying
+#			CPU architecture.
 # boot_args_in defaults to value returned by the per_version_boot_params
 #			shell function.
 #
