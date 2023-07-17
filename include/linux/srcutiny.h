2b552d0dc3ffe71d912043f7dccf4b80af3fefbf
Paul E. McKenney <paulmck@kernel.org>
2023-07-17 11:45:42 -0700

    fixup! srcu,notifier: Remove #ifdefs in favor of SRCU Tiny srcu_usage
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- include/linux/srcutiny.h
+++ include/linux/srcutiny.h
@@ -49,11 +49,8 @@ void srcu_drive_gp(struct work_struct *wp);
 	static struct srcu_struct name = __SRCU_STRUCT_INIT(name, name, name)
 
 // Dummy structure for srcu_notifier_head.
-struct srcu_usage {
-	char srcuu_dummy;
-};
-
-#define __SRCU_USAGE_INIT(name) { .srcuu_dummy = 0, }
+struct srcu_usage { };
+#define __SRCU_USAGE_INIT(name) { }
 
 void synchronize_srcu(struct srcu_struct *ssp);
 
