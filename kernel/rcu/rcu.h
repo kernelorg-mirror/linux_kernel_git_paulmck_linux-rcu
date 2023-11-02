9ae46e9debca346cb1d3aa8f31a7358c39a18ce0
Paul E. McKenney <paulmck@kernel.org>
2023-11-02 09:51:41 -0700

    fixup! rcu: Restrict access to RCU CPU stall notifiers
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- kernel/rcu/rcu.h
+++ kernel/rcu/rcu.h
@@ -262,10 +262,11 @@ static inline bool rcu_stall_is_suppressed_at_boot(void)
 	return rcu_cpu_stall_suppress_at_boot && !rcu_inkernel_boot_has_ended();
 }
 
+extern int rcu_cpu_stall_notifiers;
+
 #ifdef CONFIG_RCU_STALL_COMMON
 
 extern int rcu_cpu_stall_ftrace_dump;
-extern int rcu_cpu_stall_notifiers;
 extern int rcu_cpu_stall_suppress;
 extern int rcu_cpu_stall_timeout;
 extern int rcu_exp_cpu_stall_timeout;
--- kernel/rcu/update.c
+++ kernel/rcu/update.c
@@ -538,11 +538,12 @@ long torture_sched_setaffinity(pid_t pid, const struct cpumask *in_mask)
 EXPORT_SYMBOL_GPL(torture_sched_setaffinity);
 #endif
 
+int rcu_cpu_stall_notifiers __read_mostly; // !0 = provide stall notifiers (rarely useful)
+EXPORT_SYMBOL_GPL(rcu_cpu_stall_notifiers);
+
 #ifdef CONFIG_RCU_STALL_COMMON
 int rcu_cpu_stall_ftrace_dump __read_mostly;
 module_param(rcu_cpu_stall_ftrace_dump, int, 0644);
-int rcu_cpu_stall_notifiers __read_mostly; // !0 = provide stall notifiers (rarely useful)
-EXPORT_SYMBOL_GPL(rcu_cpu_stall_notifiers);
 #ifdef CONFIG_RCU_CPU_STALL_NOTIFIER
 module_param(rcu_cpu_stall_notifiers, int, 0444);
 #endif // #ifdef CONFIG_RCU_CPU_STALL_NOTIFIER
