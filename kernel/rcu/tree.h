2cfd866c41d499797686dac4fe17b7b6d69d0fdf
Frederic Weisbecker <frederic@kernel.org>
2023-12-08 23:05:43 +0100

    rcu/exp: Make parallel exp gp kworker per rcu node
    When CONFIG_RCU_EXP_KTHREAD=n, the expedited grace period per node
    initialization is performed in parallel via workqueues (one work per
    node).
    
    However in CONFIG_RCU_EXP_KTHREAD=y, this per node initialization is
    performed by a single kworker serializing each node initialization (one
    work for all nodes).
    
    The second part is certainly less scalable and efficient beyond a single
    leaf node.
    
    To improve this, expand this single kworker into per-node kworkers. This
    new layout is eventually intended to remove the workqueues based
    implementation since it will essentially now become duplicate code.
    
    Signed-off-by: Frederic Weisbecker <frederic@kernel.org>
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- kernel/rcu/tree.h
+++ kernel/rcu/tree.h
@@ -72,6 +72,7 @@ struct rcu_node {
 				/* Online CPUs for next expedited GP. */
 				/*  Any CPU that has ever been online will */
 				/*  have its bit set. */
+	struct kthread_worker *exp_kworker;
 	unsigned long cbovldmask;
 				/* CPUs experiencing callback overload. */
 	unsigned long ffmask;	/* Fully functional CPUs. */
