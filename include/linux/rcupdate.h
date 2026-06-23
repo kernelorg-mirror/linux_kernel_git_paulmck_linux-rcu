fdc88291364bd83c85eb7e54be0e24a255b472c4
Puranjay Mohan <puranjay@kernel.org>
2026-06-23 13:10:15 -0700

    rcu: Rename struct rcu_gp_oldstate to rcu_gp_seq
    The polled grace-period state structure rcu_gp_oldstate holds a snapshot
    of the normal (and, on SMP, expedited) grace-period sequence numbers.
    Upcoming changes store this structure in the callback segment list, where
    the "oldstate" name reads poorly: there it represents the grace period a
    segment is waiting on and is also compared against the current
    grace-period state.
    
    Rename struct rcu_gp_oldstate to the more neutral struct rcu_gp_seq, and
    shorten its members rgos_norm and rgos_exp to norm and exp.  Local
    variables and parameters of this type are renamed from rgosp/rgos to
    gsp/gs accordingly.
    
    No functional change.
    
    Signed-off-by: Puranjay Mohan <puranjay@kernel.org>
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- include/linux/rcupdate.h
+++ include/linux/rcupdate.h
@@ -52,9 +52,9 @@ void call_rcu(struct rcu_head *head, rcu_callback_t func);
 void rcu_barrier_tasks(void);
 void synchronize_rcu(void);
 
-struct rcu_gp_oldstate;
+struct rcu_gp_seq;
 unsigned long get_completed_synchronize_rcu(void);
-void get_completed_synchronize_rcu_full(struct rcu_gp_oldstate *rgosp);
+void get_completed_synchronize_rcu_full(struct rcu_gp_seq *gsp);
 
 // Maximum number of unsigned long values corresponding to
 // not-yet-completed RCU grace periods.
